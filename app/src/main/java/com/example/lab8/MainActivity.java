3ae301a8d333cf7abead858b93a0d9cf9f1d2386
“Areeba <areeba.fazalxx11@gmail.com>
2022-10-25 17:43:35 -0600

    implemented addCity function

--- app/src/main/java/com/example/lab8/MainActivity.java
+++ app/src/main/java/com/example/lab8/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<City> cityDataList;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
